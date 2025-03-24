22798007435363b751836fda29bed77aa23acf16
Charlene <104850179+clcapili@users.noreply.github.com>
2025-03-24 17:51:34 -0400

    Update load.js

--- public/js/load.js
+++ public/js/load.js
@@ -3,9 +3,9 @@ let slickSliders = document.querySelectorAll('.slick-slider-wrapper');
 if (slickSliders.length > 0) {
 	for (let i = 0; i < slickSliders.length; i++) {
         if (!__.dom.closest(slickSliders[i], '.tab-pane')) {
-            new HHA.SlickSlider(slickSliders[i]);
+            new Project.SlickSlider(slickSliders[i]);
         } else if (__.dom.closest(slickSliders[i], '.tab-pane.active')) {
-            new HHA.SlickSlider(slickSliders[i]);
+            new Project.SlickSlider(slickSliders[i]);
         }
     }
 }
